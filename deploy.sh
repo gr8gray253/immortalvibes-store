8015a10016dae59697da84e529e3431bca68e034
Eric Gray <ericgray928@live.com>
2026-04-11 23:17:45 -0700

    ci: fix deploy target to immortalvibes-store (live account project)

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ set -e
 echo "Building..."
 cd web && npm run build
 echo "Deploying..."
-CLOUDFLARE_ACCOUNT_ID=e033027ade765b35fd428536f7c989af wrangler pages deploy .svelte-kit/cloudflare --project-name immortalvibes --commit-dirty=true
+CLOUDFLARE_ACCOUNT_ID=e033027ade765b35fd428536f7c989af wrangler pages deploy .svelte-kit/cloudflare --project-name immortalvibes-store --commit-dirty=true
 echo "Done! https://immortalvibes.pages.dev"
